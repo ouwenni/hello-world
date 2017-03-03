692799b1d80f38994e1e3f6af1ad33e04badbcc0
ouwenni <85857828@qq.com>
2017-03-03 14:05:07 +0800

    Add files via upload

---
+++ Slicebox/js/index.js
@@ -0,0 +1,14 @@
+btnclick = function(){
+	var dia = document.getElementById("all");
+	dia.style.display = "none";
+	document.getElementById("qr_dialog").style.display = "none";
+}
+
+
+nextclick = function(){
+	alert("next");
+}
+
+preclick = function(){
+	alert("pre");
+}
